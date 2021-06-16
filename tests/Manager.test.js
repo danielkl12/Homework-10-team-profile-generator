dfef65f56fd3bd777f17c70d0093020266d7cb72
Daniel Klauer <danielkl12@icloud.com>
2021-06-16 13:25:48 -0500

    initial commit---file structure, html

---
+++ tests/Manager.test.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./type/Manager");
+const Engineer = require("./type/Engineer");
+const Intern = require("./type/Intern");
+
+const employees = [];
+
